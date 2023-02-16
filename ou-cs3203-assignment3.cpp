4aac7c8800a98c99326ec6139951238af0e86f5a
Abe Artus <abraham.a.artus-1@ou.edu>
2023-02-16 14:10:11 -0600

    File created, and listSum Method added

---
+++ ou-cs3203-assignment3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include  <vector>
+using namespace std;
+ 
+int sumList(vector<int> arr);
+
+
+
+// Create a software program (in any language you wish) which contains a function / method which
+// computes the sum of a list / array of numbers (integers, whatever you want) given as a parameter and
+// returns the result. Commit the result.
+int sumList(vector<int> arr ) {
+    int sum = 0;
+    for (int i = 0; i < arr.size() ; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
