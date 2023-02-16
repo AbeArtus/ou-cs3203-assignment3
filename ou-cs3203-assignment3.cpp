fa39523315a5d428b2542386977d6e8ac4a8a1e7
Abe Artus <abraham.a.artus-1@ou.edu>
2023-02-16 14:28:51 -0600

    Main added and implemented

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -5,7 +5,28 @@ using namespace std;
 int sumList(vector<int> arr);
 int multiplyList(vector<int> arr);
 
+int main() {
+    vector<int> list;  
 
+    cout << "Enter vectors numbers seperated by spaces, end seq. with E:";
+    int n;
+    while (cin >> n) {  
+        list.push_back(n);  
+    }
+    
+
+    cout << "Vector has" << list.size() << " numbers: ";
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Sum of the vector Integers:" << sumList(list) << endl;
+
+    cout << "Product of the vector Integers:" << multiplyList(list) << endl;
+
+    return 0;
+}
 
 
 // Create a software program (in any language you wish) which contains a function / method which
