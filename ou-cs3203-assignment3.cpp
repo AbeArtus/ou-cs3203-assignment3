a052c721a34527f97c0f1d51f46f347f10aaf2c9
Abe Artus <abraham.a.artus-1@ou.edu>
2023-02-16 21:33:37 -0600

    5) reverse method, and print method (extra :p)

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -4,46 +4,64 @@ using namespace std;
  
 int sumList(vector<int> arr);
 int multiplyList(vector<int> arr);
+void print(vector<int> arr);
+vector<int> reverse(vector<int> vec);
 
 int main() {
+    // init vector to be used
     vector<int> list;  
 
+    // prompt user to enter Integers
     cout << "Enter vectors numbers seperated by spaces, end seq. with E:";
     int n;
     while (cin >> n) {  
         list.push_back(n);  
     }
     
-
-    cout << "Vector has" << list.size() << " numbers: ";
-    for (int i = 0; i < list.size(); i++) {
-        cout << list[i] << " ";
-    }
-    cout << endl;
-
+    // display the vector size and elements
+    print(list);
     cout << "Sum of the vector Integers:" << sumList(list) << endl;
-
     cout << "Product of the vector Integers:" << multiplyList(list) << endl;
 
     return 0;
 }
 
+void print(vector<int> arr) {
+   cout << "Vector has " << arr.size() << " numbers: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
-// Create a software program (in any language you wish) which contains a function / method which
-// computes the sum of a list / array of numbers (integers, whatever you want) given as a parameter and
-// returns the result. Commit the result.
+// computes the sum of a list / array of numbers given as a parameter and returns the result.
 int sumList(vector<int> arr ) {
     int sum = 0;
+    // itterate and add each integer to sum
     for (int i = 0; i < arr.size() ; i++) {
         sum += arr[i];
     }
     return sum;
 }
 
+
+// compute the 
 int multiplyList(vector<int> arr) {
     int prod = 1;
+    // multyply each elemnt times prod and save to product
     for (int i = 0; i < arr.size()  ; i++) {
         prod = arr[i]*prod;
     }
     return prod;
+}
+
+vector<int> reverse(vector<int> vec){
+    int temp = 0;
+    for (int i = 0; i < (int)(vec.size() / 2); i++) {
+        temp = vec[i];
+        vec[i] = vec[vec.size() -1];
+        vec[vec.size() -1] = temp;
+    }
+    return vec;
+
 }
